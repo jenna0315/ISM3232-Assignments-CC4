f80f2a40299369cf7887f2f740afbf5521ce59a4
Your Name <you@example.com>
2024-09-15 16:01:09 -0400

    Task 2 Create displayProductDetails Function

--- inventory_management.js
+++ inventory_management.js
@@ -6,3 +6,25 @@ const inventory = [
     {name:"Speaker", price:500, quantity:30,lowStockLevel:5},
     {name:"Apple Watch", price:700, quantity:2,lowStockLevel:5}
 ];
+
+//Task 2 Create displayProductDetails function
+console.log(`Initial Inventory Summary:`)//better organization of console display
+function displayProductDetails (inventory){
+    return inventory.forEach((item)=>
+{console.log(`${item.name} 
+Price:$${item.price} 
+${item.quantity} units remaining
+Minimum Stock: ${item.lowStockLevel}`)}
+)};
+let productDetails = displayProductDetails(inventory);//used to display product details in a nice format
+
+console.log(`Initial Test of Stock Levels:`)//better organization of console display
+function determineStock (inventory) {
+{let stockStatus = inventory.quantity <= inventory.lowStockLevel ? `Low Stock` : `In Stock`;
+   return `The ${inventory.name} is ${stockStatus}.`}};//function to reflect stock levels
+
+console.log(`${determineStock(inventory[0])}`);
+console.log(`${determineStock(inventory[1])}`);
+console.log(`${determineStock(inventory[2])}`);
+console.log(`${determineStock(inventory[3])}`);
+console.log(`${determineStock(inventory[4])}`);
