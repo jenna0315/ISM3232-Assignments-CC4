68f77956e26f3f00e8dfa22bc78420e59931585f
Your Name <you@example.com>
2024-09-15 16:00:11 -0400

    Task 1 Initialize Inventory with Product Objects

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ISM3232-Assignments-CC4
+A store manager needs a system that tracks products in the inventory, updates stock levels after sales, and alerts when stock levels are low. Additionally, the system should calculate the total value of the remaining stock for each product type.
---
+++ inventory_management.js
@@ -0,0 +1,8 @@
+//Task 1 Initialize inventory with product objects
+const inventory = [
+    {name:"Laptop", price:1000, quantity:22,lowStockLevel:7},
+    {name:"IPhone 15",price:1200, quantity:25,lowStockLevel:6},
+    {name:"Tablet", price:600, quantity:3,lowStockLevel:4},
+    {name:"Speaker", price:500, quantity:30,lowStockLevel:5},
+    {name:"Apple Watch", price:700, quantity:2,lowStockLevel:5}
+];
